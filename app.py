25f80b81b758f27ba460242dfee7200dd19247d9
Aurelien Jacomy <jacomya@gmail.com>
2025-11-15 15:48:28 -0300

    test simple version

--- app.py
+++ app.py
@@ -8,9 +8,10 @@ from geopy.extra.rate_limiter import RateLimiter
 '''
 # TaxiFareModel 
 '''
-
+st.write("Hello!")
 #url = 'https://taxifare.lewagon.ai/predict'
-url = 'https://taxifare-495542883015.southamerica-east1.run.app/predict'
+
+'''url = 'https://taxifare-495542883015.southamerica-east1.run.app/predict'
 
 if url == 'https://taxifare.lewagon.ai/predict':
 
@@ -33,8 +34,8 @@ with st.form(key='my_taxifare'):
     submitted = st.form_submit_button(label='Calculate my taxifare')
 
 # Show Result    
-if submitted:
-    # Instantiate Geolocator and get lat long of pickup and dropoff
+#if submitted:
+   # Instantiate Geolocator and get lat long of pickup and dropoff
     geolocator = Nominatim(user_agent="taxifare")
     geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
     pickup_location = geocode(pickup_address)
@@ -46,7 +47,7 @@ if submitted:
         request_url = url+f"?pickup_datetime={date}%20{time}&pickup_longitude={pickup_location.longitude}&pickup_latitude={pickup_location.latitude}&dropoff_longitude={dropoff_location.longitude}&dropoff_latitude={dropoff_location.latitude}&passenger_count={passenger_count}"
         response = requests.get(request_url)
         st.text("")
-        st.write(f'''#### 🚖 Your taxi fare will cost around {round(response.json()['fare'], 2)} USD.''')
+        st.write(f'#### 🚖 Your taxi fare will cost around {round(response.json()['fare'], 2)} USD.')
         
         # Show map with pickup and dropoff locations
         df = pd.DataFrame({'lat': [pickup_location.latitude, dropoff_location.latitude], 'lon': [pickup_location.longitude, dropoff_location.longitude]})
@@ -54,3 +55,5 @@ if submitted:
     
     else:
         st.write("Couldn't find pickup or dropoff address")
+
+'''
